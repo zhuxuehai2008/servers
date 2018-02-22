550e7b2a5ca9a1c227552b83c3fcfb3e83c4b6d6
zhuxuehai2008 <zhuxuehai2008@126.com>
2018-02-22 17:31:45 +0800

    1

--- server/src/main/java/com/zxh/core/exception/GlobalException.java
+++ server/src/main/java/com/zxh/core/exception/GlobalException.java
@@ -3,6 +3,8 @@ package com.zxh.core.exception;
 import java.util.HashMap;
 
 public class GlobalException extends Exception{
+	public Integer status;
+	public String message;
 	public GlobalException() {
 		super();
 	}
